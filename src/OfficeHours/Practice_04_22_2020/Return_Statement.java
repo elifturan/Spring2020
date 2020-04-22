c3f87b335e7c975a93216bdca2b347c764fd1bd0
elifturan <elifergucluuu@gmail.com>
2020-04-22 16:50:44 -0400

    Practice

---
+++ src/OfficeHours/Practice_04_22_2020/Return_Statement.java
@@ -0,0 +1,21 @@
+package OfficeHours.Practice_04_22_2020;
+
+public class Return_Statement {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Batch 18");
+        method1();
+
+    }
+    public static void method1(){
+        if(10>9){
+
+           System.exit(0);
+        }
+        System.out.println("Hello Cybertek");
+    }
+
+
+
+}
