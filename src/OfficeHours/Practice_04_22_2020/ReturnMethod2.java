6dca4c192150e733433866a4c5c7bb50da7d771a
elifturan <elifergucluuu@gmail.com>
2020-04-22 16:14:29 -0400

    Practice

---
+++ src/OfficeHours/Practice_04_22_2020/ReturnMethod2.java
@@ -0,0 +1,42 @@
+package OfficeHours.Practice_04_22_2020;
+
+import java.util.Arrays;
+
+public class ReturnMethod2 {
+    public static void main(String[] args) {
+  int [] arr ={100, 40 , 500, 50 ,30,400,-5,-100};
+        Arrays.sort(arr);//ascending order!
+
+        System.out.println(Arrays.toString(arr));
+  int lastIndexNum= arr.length-1;
+   int secondLastIndexNum=lastIndexNum-1;//arr.length-2 same thing
+   //                    = arr.length-1-1= arr.length -2
+      int secondMAxNum= arr[secondLastIndexNum];
+
+        System.out.println(secondMAxNum);
+
+
+        int [] arr2 ={1,2,3,4,5,6,7,8};
+      int num2 =  secondMAx(arr2);
+        System.out.println(num2);
+    }
+public static int secondMAx (int[] arr){
+
+    Arrays.sort(arr);//ascending order!
+
+    System.out.println(Arrays.toString(arr));
+    int lastIndexNum= arr.length-1;
+    int secondLastIndexNum=lastIndexNum-1;//arr.length-2 same thing
+    //                    = arr.length-1-1= arr.length -2
+    int secondMAxNum= arr[secondLastIndexNum];
+
+
+
+        return secondMAxNum ;
+}
+
+
+
+
+
+}
