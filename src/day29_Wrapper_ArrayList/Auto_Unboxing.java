9615ca30c1ac0bbe652b101afe1330947e057bb4
elifturan <elifergucluuu@gmail.com>
2020-04-27 10:41:51 -0400

    Day29 Class

---
+++ src/day29_Wrapper_ArrayList/Auto_Unboxing.java
@@ -0,0 +1,18 @@
+package day29_Wrapper_ArrayList;
+
+public class Auto_Unboxing {
+    public static void main(String[] args) {
+
+      int a =100;
+   Integer b = a; //Autobaxing process ; converts primitive into wrapper class object
+
+        Byte b1 = 98;
+        short s1 =b1;//unboxing
+        byte b2 = b1;//unboxing
+        int i1 = b1;//unboxing
+
+
+
+
+    }
+}
