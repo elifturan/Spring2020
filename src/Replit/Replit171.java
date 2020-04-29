163e7efb3b90061f4c1858a8b7f0a3e76bcbeb24
elifturan <elifergucluuu@gmail.com>
2020-04-28 22:08:28 -0400

    Day30 Class

--- src/Replit/Replit171.java
+++ src/Replit/Replit171.java
@@ -4,19 +4,10 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Replit171 {
-    public static boolean isPalindrome (String check){
-          String result ="";
-        for (int i = check.length()-1; i >=0 ; i--) {
-        result += check.charAt(i);
-        }
-if(check.equalsIgnoreCase(result)){
-    return true;
-}
-return  false;
+    public static String [] reverse(String [] arr){
+
+        return arr;
     }
 
-    public static void main(String[] args) {
 
-
-    }
 }
