7a4583dc872239bd9f0d581f71780074a872c627
elifturan <elifergucluuu@gmail.com>
2020-04-30 11:09:35 -0400

    Day 31 Class

--- src/day31_Bulk_Operation/ArrayList_Methods.java
+++ src/day31_Bulk_Operation/ArrayList_Methods.java
@@ -8,26 +8,22 @@ import java.util.Collections;
 public class ArrayList_Methods {
     public static void main(String[] args) {
 
-        ArrayList<String>list1= new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>();
         list1.add("A");
         list1.add("B");
         list1.add("C");
         list1.add("D");
 
-     boolean r1 =   list1.contains("Z");//it doesn't contains Z so it returns false
-        boolean r2 =   list1.contains("C");// it contains C so it returns true
-
-
-
+        boolean r1 = list1.contains("Z");//it doesn't contains Z so it returns false
+        boolean r2 = list1.contains("C");// it contains C so it returns true
 
 
         System.out.println(r1);
-        ArrayList<String>list2= new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
         list1.add("A");
         list1.add("C");
         list1.add("B");
         list1.add("D");
-
 
 
         Collections.sort(list1);//{A,B,C,D}
@@ -37,23 +33,14 @@ public class ArrayList_Methods {
         System.out.println(b1);//false
 
 
-
-
-
-
-
-
-
-
-
         //Arrays.equal(arr1,arr2)== Array equals method other ArrayList method!
         System.out.println("================================");
 
-        int [] arr1 ={1,2,3};
-        int [] arr2 ={3,2,1};
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {3, 2, 1};
         Arrays.sort(arr1);
         Arrays.sort(arr2);//{1,2,3}
-        boolean b2 = Arrays.equals(arr1,arr2);
+        boolean b2 = Arrays.equals(arr1, arr2);
         System.out.println(b2);
 
         System.out.println("==================================");
@@ -67,10 +54,10 @@ public class ArrayList_Methods {
 
         Collections.sort(list3);
         System.out.println(list3);
-ArrayList<Integer> reversedList3 =new ArrayList<>();
+        ArrayList<Integer> reversedList3 = new ArrayList<>();
 
-        for (int i = list3.size()-1; i >= 0 ; i--) {
-           // System.out.print(list3.get(i)+" ");
+        for (int i = list3.size() - 1; i >= 0; i--) {
+            // System.out.print(list3.get(i)+" ");
 
             reversedList3.add(list3.get(i));
         }
@@ -78,8 +65,11 @@ ArrayList<Integer> reversedList3 =new ArrayList<>();
         list3 = reversedList3;
         System.out.println(list3);
 
+        System.out.println(list1.size());
+        list3.clear();// removes everything
 
-
+        boolean r4 = list3.isEmpty();
+        System.out.println(r4);
 
     }
 }
