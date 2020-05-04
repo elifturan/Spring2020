708f280763687831f6b5fe7a7e0c19e52287cd9e
elifturan <elifergucluuu@gmail.com>
2020-05-04 10:28:15 -0400

    Day 32 Class

---
+++ src/day33_CustomClass/Duplicates_Warmup.java
@@ -0,0 +1,28 @@
+package day33_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Duplicates_Warmup {
+    public static void main(String[] args) {
+        /*
+        2. write a program that can return the duplicated objects from a an ArrayList of Integers
+			Ex:
+				list: {1,1,2,3,4,5,5}
+				sout(list); ==> {1,1,5,5};
+		Note: 1. DO NOT use any extra arrayList
+			  2. DO NOT use any loops
+			  3. DO NOT use any sort method
+			  4. Use predicate and collections methods only
+         */
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,2,3,4,5,5));
+        list.removeIf(p-> Collections.frequency(list,p)==1);
+        System.out.println(list);
+
+
+
+    }
+}
