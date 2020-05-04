4f2d853121599606c9abe9baf6984d0ed17d8519
elifturan <elifergucluuu@gmail.com>
2020-05-04 11:54:14 -0400

    Day 33 Class

---
+++ src/day33_CustomClass/Car_CustomClass.java
@@ -0,0 +1,18 @@
+package day33_CustomClass;
+
+public class Car_CustomClass {
+    //instance variable: object variables
+    // declared veribale: vareiables that are declared outside a block
+    // local variable: variables that are declared within a block
+
+    String Brand;
+    String Model;
+    int year ;
+    String color;
+
+    public static void main(String[] args) {
+        int a = 100 ; // local variable
+
+
+    }
+}
