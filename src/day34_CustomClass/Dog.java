d83819fa0caf002498dae785acb981dfc42c6ba7
elifturan <elifergucluuu@gmail.com>
2020-05-05 11:53:16 -0400

    Day 34 Class

--- src/day34_CustomClass/Dog.java
+++ src/day34_CustomClass/Dog.java
@@ -6,13 +6,24 @@ public class Dog {
     int age ;
     String color;
 String name;
-public void setDogInfo(String dogBreed,String dogSize, int dogAge, String dogColor,String dogName){
-    breed = dogBreed;
-    size=dogSize;
-    age=dogAge;
-    color=dogColor;
-    name = dogName;
+public void setDogInfo(String breed,String size, int age, String color,String name){
+    this.breed = breed;
+    this.size=size;
+    this.age=age;
+    this.color=color;
+    this.name = name;
+    //this keyword is used for calling object instance variable
 }
+public void eat(String dogFood){
 
-
+}
+public void sleep(){
+    System.out.println(name+" is sleeping");
+}public void drink(){
+        System.out.println(name+ " is drinking");
+    }
+public String toString(){
+    return "Breed is: "+breed+
+    "\nName is: "+name+"\nSize is: "+size+"\nAge is: "+age+"\nColor is: "+color;
+}
 }
---
+++ src/day34_CustomClass/DogObject.java
@@ -0,0 +1,38 @@
+package day34_CustomClass;
+
+public class DogObject {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.setDogInfo("Husky","small",1,"yellow","Bestie");
+        System.out.println(dog1);
+   /*    dog1.breed="Husky";
+       dog1.size ="Small";
+       dog1.color="yellow";
+       dog1.name="Bestie";
+       dog1.age=1;
+
+
+    */
+
+        System.out.println(dog1.breed);
+        System.out.println(dog1.size);
+        System.out.println(dog1.color);
+        System.out.println(dog1.name);
+        System.out.println(dog1.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
