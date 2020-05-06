33c5265f78fcb73b1c2dc340c2509755ce43baac
elifturan <elifergucluuu@gmail.com>
2020-05-06 15:38:41 -0400

    Office Hours

--- src/OfficeHours/Practice_05_06_2020/DevelopersObject.java
+++ src/OfficeHours/Practice_05_06_2020/DevelopersObject.java
@@ -1,6 +1,8 @@
 package OfficeHours.Practice_05_06_2020;
 
 import javax.swing.event.DocumentEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DevelopersObject {
     public static void main(String[] args) {
@@ -18,12 +20,28 @@ dev2.fixingBug();
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Developer[] arr ={dev1,dev2};
         for (int i = 0; i <arr.length ; i++) {
-            System.out.println(arr[i]);
+            System.out.println(arr[i].name);
         }
 
         for(Developer each:arr){
-            System.out.println(each);
+            System.out.println(each.name+" "+each.salary);
         }
+        ArrayList<Developer> developerList = new ArrayList<>(Arrays.asList(arr));
+        //developerList.addAll(Arrays.asList(dev1,dev2));
+
+        for (int i = 0; i <developerList.size() ; i++) {
+            System.out.println(developerList.get(i).name);
+        }
+
+for (Developer each : developerList){
+    System.out.println(each.name+" "+each.salary);
+}
+
+
+
+
+
+
 
 
     }
